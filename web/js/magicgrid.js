e30765ae26b8283baa27bd334d1d95b8f79b7a49
anna mosiagina <anna.m@namecheap.com>
2020-07-31 11:07:59 +0300

    updated templates

---
+++ web/js/magicgrid.js
@@ -0,0 +1,42 @@
+let magicGrid = new MagicGrid({
+    container: '.m-grid',
+    animate: false,
+    gutter: 28,
+    static: true,
+    center: true
+
+});
+
+
+
+magicGrid.listen();
+
+
+
+let faqMagicGrid = new MagicGrid({
+    container: '.m-grid-faq',
+    animate: false,
+    gutter: 28,
+    static: true,
+    center: true
+
+});
+
+
+
+faqMagicGrid.listen();
+
+
+
+let revMagicGrid = new MagicGrid({
+    container: '.m-grid-rev',
+    animate: false,
+    gutter: 28,
+    static: true,
+    center: true
+
+});
+
+
+
+revMagicGrid.listen();
